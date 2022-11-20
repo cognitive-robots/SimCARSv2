d3df5c0fcb2bd960e9baa3ab43cc1c985ed81e23
Rhys Howard <rhyshoward@live.com>
2022-11-20 22:02:23 +0000

    CU-31wfpn1[in-progress]: Replaced smart pointers in qt5 map scene widget test executable and got it compiled and running with no seg. faults or memory leaks

--- src/qmap_scene_widget_test/qmap_scene_widget_test.cpp
+++ src/qmap_scene_widget_test/qmap_scene_widget_test.cpp
@@ -13,7 +13,7 @@
 
 using namespace ori::simcars;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
@@ -21,19 +21,19 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    std::shared_ptr<const geometry::TrigBuff> trig_buff = geometry::TrigBuff::init_instance(360000, geometry::AngleType::RADIANS);
+    geometry::TrigBuff const *trig_buff = geometry::TrigBuff::init_instance(360000, geometry::AngleType::RADIANS);
 
     QApplication app(argc, argv);
 
     std::cout << "Beginning map load" << std::endl;
 
-    std::shared_ptr<const map::IMap<std::string>> map;
+    map::IMap<std::string> const *map;
 
     try
     {
         map = map::lyft::LyftMap::load(argv[1]);
     }
-    catch (const std::exception& e)
+    catch (std::exception const &e)
     {
         std::cerr << "Exception occured during map load:" << std::endl << e.what() << std::endl;
         return -1;
@@ -43,13 +43,13 @@ int main(int argc, char* argv[])
 
     std::cout << "Beginning scene load" << std::endl;
 
-    std::shared_ptr<const agent::IScene> scene;
+    agent::IScene const *scene;
 
     try
     {
         scene = agent::lyft::LyftScene::load(argv[2]);
     }
-    catch (const std::exception& e)
+    catch (std::exception const &e)
     {
         std::cerr << "Exception occured during scene load:" << std::endl << e.what() << std::endl;
         return -1;
@@ -57,22 +57,22 @@ int main(int argc, char* argv[])
 
     std::cout << "Finished scene load" << std::endl;
 
-    std::shared_ptr<structures::IStackArray<std::string>> focal_entities(
-                new structures::stl::STLStackArray<std::string>());
+    structures::IStackArray<std::string> *focal_entities =
+            new structures::stl::STLStackArray<std::string>();
 
-    std::shared_ptr<structures::IArray<std::shared_ptr<const agent::IEntity>>> entities =
+    structures::IArray<agent::IEntity const*> *entities =
             scene->get_entities();
 
     for (size_t i = 0; i < entities->count(); ++i)
     {
-        std::shared_ptr<const agent::IEntity> entity = (*entities)[i];
+        agent::IEntity const *entity = (*entities)[i];
         try
         {
-            std::shared_ptr<const agent::IValuelessConstant> ego_valueless_constant =
+            agent::IValuelessConstant const *ego_valueless_constant =
                     entity->get_constant_parameter(entity->get_name() + ".ego");
 
-            std::shared_ptr<const agent::IConstant<bool>> ego_constant =
-                    std::dynamic_pointer_cast<const agent::IConstant<bool>>(ego_valueless_constant);
+            agent::IConstant<bool> const *ego_constant =
+                    dynamic_cast<agent::IConstant<bool> const*>(ego_valueless_constant);
 
             if (ego_constant->get_value())
             {
@@ -85,18 +85,18 @@ int main(int argc, char* argv[])
         }
     }
 
-    std::shared_ptr<QFrame> frame(new QFrame());
+    QFrame *frame = new QFrame();
     frame->setWindowTitle("QMapSceneWidget Test");
     frame->setFixedSize(1000, 1000);
     frame->show();
 
-    std::shared_ptr<visualisation::QMapSceneWidget<std::string>> map_scene_widget(
-                new visualisation::QMapSceneWidget<std::string>(
-                    map,
-                    scene,
-                    frame.get(),
-                    QPoint(20, 20),
-                    QSize(960, 960)));
+    visualisation::QMapSceneWidget<std::string> *map_scene_widget =
+            new visualisation::QMapSceneWidget<std::string>(
+                map,
+                scene,
+                frame,
+                QPoint(20, 20),
+                QSize(960, 960));
     map_scene_widget->set_focal_entities(focal_entities);
     map_scene_widget->show();
 
