6f209a74365079e8140af57603acc648d70359eb
Rhys Howard <rhyshoward@live.com>
2023-01-12 11:42:10 +0000

    CU-3414jk8[review]: Added ability for causal discovery executable to export discovered causal links in JSON format

--- src/highd_json_meta_causal_discovery/highd_json_meta_causal_discovery.cpp
+++ src/highd_json_meta_causal_discovery/highd_json_meta_causal_discovery.cpp
@@ -7,6 +7,8 @@
 
 #include <rapidjson/document.h>
 #include <rapidjson/istreamwrapper.h>
+#include <rapidjson/ostreamwrapper.h>
+#include <rapidjson/writer.h>
 
 #include <iostream>
 #include <exception>
@@ -14,8 +16,6 @@
 #include <fstream>
 #include <filesystem>
 
-#define ATE_THRESHOLD 0.05f
-#define BRANCH_COUNT 100
 #define CONTROLLER_LOOKAHEAD_STEPS 10
 
 using namespace ori::simcars;
@@ -23,41 +23,63 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 5)
     {
-        std::cerr << "Usage: ./highd_json_meta_ate_test json_meta_file_path raw_data_directory_path" << std::endl;
+        std::cerr << "Usage: ./highd_json_meta_causal_discovery ate_threshold branch_count input_json_meta_file_path raw_data_directory_path [output_json_meta_file_path]" << std::endl;
         return -1;
     }
 
-    std::string json_meta_file_path_str = argv[1];
-    std::filesystem::path json_meta_file_path(json_meta_file_path_str);
+    FP_DATA_TYPE ate_threshold = std::atof(argv[1]);
+    size_t branch_count = std::atoi(argv[2]);
 
-    if (!std::filesystem::is_regular_file(json_meta_file_path))
+    std::string input_json_meta_file_path_str = argv[3];
+    std::filesystem::path input_json_meta_file_path(input_json_meta_file_path_str);
+
+    if (!std::filesystem::is_regular_file(input_json_meta_file_path))
     {
-        throw std::invalid_argument("JSON meta file path '" +
-                                    json_meta_file_path_str +
+        throw std::invalid_argument("Input JSON meta file path '" +
+                                    input_json_meta_file_path_str +
                                     "' does not indicate a valid file");
     }
 
-    std::ifstream json_meta_filestream(json_meta_file_path);
-    rapidjson::BasicIStreamWrapper json_meta_stream(json_meta_filestream);
+    std::string output_json_meta_file_path_str;
+    std::filesystem::path output_json_meta_file_path;
+
+    if (argc > 5)
+    {
+        output_json_meta_file_path_str = argv[5];
+        output_json_meta_file_path = std::filesystem::path(output_json_meta_file_path_str);
+        std::filesystem::path output_json_meta_file_path_dir = output_json_meta_file_path.parent_path();
+
+        if (!std::filesystem::is_directory(output_json_meta_file_path_dir) ||
+                std::filesystem::is_directory(output_json_meta_file_path))
+        {
+            throw std::invalid_argument("Output JSON meta file path '" +
+                                        output_json_meta_file_path_str +
+                                        "' does not indicate a valid location to output a file");
+        }
+    }
+
+    std::ifstream input_json_meta_filestream(input_json_meta_file_path);
+    rapidjson::IStreamWrapper input_json_meta_stream(input_json_meta_filestream);
 
     rapidjson::Document json_meta_document;
-    json_meta_document.ParseStream(json_meta_stream);
+    json_meta_document.ParseStream(input_json_meta_stream);
 
     uint32_t scene_id = json_meta_document["scene_id"].GetUint();
     uint32_t convoy_head_id = json_meta_document["convoy_head_id"].GetUint();
     uint32_t convoy_tail_id = json_meta_document["convoy_tail_id"].GetUint();
     uint32_t independent_id = json_meta_document["independent_id"].GetUint();
 
-    std::cout << "Convoy Head Agent = non_ego_vehicle_" + std::to_string(convoy_head_id) <<
-                 std::endl;
-    std::cout << "Convoy Tail Agent = non_ego_vehicle_" + std::to_string(convoy_tail_id) <<
-                 std::endl;
-    std::cout << "Independent Agent = non_ego_vehicle_" + std::to_string(independent_id) <<
-                 std::endl;
+    std::string convoy_head_str = "non_ego_vehicle_" + std::to_string(convoy_head_id);
+    std::string convoy_tail_str = "non_ego_vehicle_" + std::to_string(convoy_tail_id);
+    std::string independent_str = "non_ego_vehicle_" + std::to_string(independent_id);
 
-    std::string raw_data_directory_path_str = argv[2];
+    std::cout << "Convoy Head Agent = " << convoy_head_str << std::endl;
+    std::cout << "Convoy Tail Agent = " << convoy_tail_str << std::endl;
+    std::cout << "Independent Agent = " << independent_str << std::endl;
+
+    std::string raw_data_directory_path_str = argv[4];
     std::filesystem::path raw_data_directory_path(raw_data_directory_path_str);
 
     std::filesystem::path recording_meta_file_path =
@@ -136,8 +158,8 @@ int main(int argc, char *argv[])
     start_time = high_resolution_clock::now();
 
     causal::ICausalDiscoverer *causal_discoverer = new causal::NecessaryDrivingCausalDiscoverer(
-                map, scene->get_time_step(), CONTROLLER_LOOKAHEAD_STEPS, ATE_THRESHOLD,
-                BRANCH_COUNT);
+                map, scene->get_time_step(), CONTROLLER_LOOKAHEAD_STEPS, ate_threshold,
+                branch_count);
 
     structures::ISet<std::pair<std::string, std::string>> *entity_causal_links =
             causal_discoverer->discover_entity_causal_links(scene, agents_of_interest);
@@ -157,6 +179,42 @@ int main(int argc, char *argv[])
         std::pair<std::string, std::string> const &entity_causal_link =
                 (*entity_causal_link_array)[i];
         std::cout << entity_causal_link.first << " -> " << entity_causal_link.second << std::endl;
+    }
+
+
+    if (argc > 5)
+    {
+        rapidjson::Value json_causal_links(rapidjson::kObjectType);
+        for (i = 0; i < entity_causal_link_array->count(); ++i)
+        {
+            std::pair<std::string, std::string> const &entity_causal_link =
+                    (*entity_causal_link_array)[i];
+            rapidjson::Value cause_str;
+            cause_str = rapidjson::StringRef(entity_causal_link.first.c_str());
+            rapidjson::Value effect_str;
+            effect_str = rapidjson::StringRef(entity_causal_link.second.c_str());
+            if (!json_causal_links.HasMember(cause_str))
+            {
+                rapidjson::Value json_causal_effects(rapidjson::kArrayType);
+                json_causal_effects.PushBack(effect_str, json_meta_document.GetAllocator());
+                json_causal_links.AddMember(cause_str, json_causal_effects,
+                                            json_meta_document.GetAllocator());
+            }
+            else
+            {
+                rapidjson::Value::Array const &json_causal_effects =
+                        json_causal_links[cause_str].GetArray();
+                json_causal_effects.PushBack(effect_str, json_meta_document.GetAllocator());
+            }
+        }
+        json_meta_document.AddMember("causal_links", json_causal_links,
+                                     json_meta_document.GetAllocator());
+
+        std::ofstream output_json_meta_filestream(output_json_meta_file_path);
+        rapidjson::OStreamWrapper output_json_meta_stream(output_json_meta_filestream);
+        rapidjson::Writer<rapidjson::OStreamWrapper> output_json_meta_writer(
+                    output_json_meta_stream);
+        json_meta_document.Accept(output_json_meta_writer);
     }
 
 
